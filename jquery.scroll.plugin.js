074d7a7de5876cad04559f89937ff48388a51a65
jiahut <jiahut@gmail.com>
2013-03-22 17:05:20 +0800

    update

--- jquery.scroll.plugin.js
+++ jquery.scroll.plugin.js
@@ -1,40 +1,43 @@
 // jquery.scroll.plugin.js
 
 /** Use Case
- var $body_scroll = $('html body').scroll()
- 
- $body_scroll.recollect()
+
+var $body = $('html body').scroll()
+$body.toggleScroll()
+
 */
 
 ;(function($,undefined){
 	var options = {
-		originalTop: 0
+		original: undefined
 	}
 
-	var $traget = null;//持有目标元素的引用
+	var $target = null;//持有目标元素的引用
 
 	var Scroll = function(self,settings){
-		$traget = self;
-		$.extend(options, settings);//向option merge参数
+		$target = self;
+		$.extend(options, settings);//向option merge settings
 		this.init();
 	}
 	Scroll.prototype.init = function(){
-		options.originalTop = $traget.scrollTop();
+		options.original = $target.scrollTop();
 		console.log(options);
 	}
 
-	Scroll.prototype.recollect = function(){
-		$traget.animate({scrollTop: options.originalTop}, 100);
-		return this.destroy();
+	Scroll.prototype.toggleScroll = function(){
+		var before = options.original
+		options.original = $target.scrollTop();
+		$target.animate({scrollTop: before}, 100);
+		return this;
 	}
 
 	Scroll.prototype.destroy = function(){
-		$traget = null;//fast to gc
-		return this;
+		$target = null;//fast to gc
+		//return this;
 	}
 
 	$.fn.scroll = function(settings){
 		var scroll = new Scroll(this,settings);
-		return this.extend(scroll);// 对象增强
+		return this.extend(scroll);// 对象增强  enhance object
 	};
 }(jQuery));
